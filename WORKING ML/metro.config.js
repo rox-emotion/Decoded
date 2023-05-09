90a42159d0aa96b86cb4e3ac57328c7f2c656955
rox-emotion <113882292+rox-emotion@users.noreply.github.com>
2023-05-09 11:30:57 +0300

    apk-works version

--- WORKING ML/metro.config.js
+++ WORKING ML/metro.config.js
@@ -1,9 +1,29 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// module.exports = async () => {
+//   const defaultConfig = await getDefaultConfig(__dirname);
+//   return {
+//     resolver: {
+//       ...defaultConfig.resolver,
+//       assetExts: [...defaultConfig.resolver.assetExts, 'bin', 'txt', 'jpg', 'ttf', 'png', 'm4a', 'mp4', 'otf'],
+//       sourceExts: [...defaultConfig.resolver.sourceExts, 'js', 'json', 'ts', 'tsx', 'jsx'],
+//     },
+//     // Add any other custom configuration options for Metro bundler here
+//   };
+// };
+
+
 const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = {
-    resolver: {
-      assetExts: ['bin', 'txt', 'jpg', 'ttf', 'png', 'm4a', 'mp4', 'otf'],
-      sourceExts: ['js', 'json', 'ts', 'tsx', 'jsx'],
-    },
-  };
+const config = getDefaultConfig(__dirname);
+
+config.resolver.assetExts.push(
+  'bin', 'txt', 'jpg', 'ttf', 'png', 'm4a', 'mp4', 'otf'
+);
+
+config.resolver.sourceExts.push(
+  'js', 'json', 'ts', 'tsx', 'jsx'
+);
+
+module.exports = config;
